b275d9721cfcbbe6aac8c093f3e3890c1289e080
MAHAM <abdulkabore@gmail.com>
2022-05-31 14:22:07 -0700

    fix error

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -5,5 +5,5 @@ def uppercase(str):
         uni_code = ord(str[i])
         if uni_code >= 97 and uni_code <= 122:
             uni_code = uni_code - 32
-        print("{uni_code}", end='')
+        print("{}".format(uni_code), end='')
     print()
