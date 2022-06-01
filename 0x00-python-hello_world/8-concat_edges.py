be1ca982667e50469df755735bc3e3598df3fcd6
MAHAM <abdulkabore@gmail.com>
2022-06-01 10:46:37 +0000

    fix error

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
-from copyreg import constructor
-
-
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str.index("h "))
-str = f"{str[39:67]}{str[107:112]}{str[114:112]}{str[:6]}"
+str = str[39:67] + str[107:112] + str[:6]
 print(str)
-
