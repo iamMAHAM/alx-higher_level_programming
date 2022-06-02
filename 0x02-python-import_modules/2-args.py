3c5176a76ac999702d9e066244a8fcb85f363915
MAHAM <abdulkabore@gmail.com>
2022-06-02 15:02:41 +0000

    try fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
-from sys import argv
+if __name__ == "__main__":
+    from sys import argv
+    length = len(argv) - 1
+    if length == 1 or length == 0:
+        print(f"{length} argument.")
+    else:
+        print(f"{length} arguments:")
 
-length = len(argv) - 1
-if length == 1 or length == 0:
-    print(f"{length} argument.")
-else:
-    print(f"{length} arguments:")
-
-for i in range(length + 1):
-    print(f"{i}: {argv[i]}") if i != 0 else 0
+    for i in range(length + 1):
+        print(f"{i}: {argv[i]}") if i != 0 else 0
