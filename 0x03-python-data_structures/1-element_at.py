e753f1c6af47be0626da2ef29ca7661871f13d45
MAHAM <abdulkabore@gmail.com>
2022-06-04 09:56:23 +0000

    remove test line

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -5,7 +5,3 @@ def element_at(my_list, idx):
         return None
     else:
         return (my_list[idx])
-
-my_list = [1, 2, 3, 4, 5]
-idx = 6
-print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
