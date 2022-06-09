6df1baf4d16b7dd20f1c44181d5f56686452fdb7
MAHAM <abdulkabore@gmail.com>
2022-06-09 09:51:48 +0200

    support now empty dicts

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@
 def best_score(a_dictionary):
     max = 0
     values = []
-    if a_dictionary is None:
+    if a_dictionary is None or len(a_dictionary) == 0:
         return None
     for val in a_dictionary.values():
         max = val if val > max else max
