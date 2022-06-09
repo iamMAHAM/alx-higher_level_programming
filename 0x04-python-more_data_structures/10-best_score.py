9376def7cef9fce65f50429bfccab809ce6cd1da
MAHAM <abdulkabore@gmail.com>
2022-06-09 09:42:59 +0200

    best score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    max = 0
+    values = []
+    if a_dictionary is None:
+        return None
+    for val in a_dictionary.values():
+        max = val if val > max else max
+    values = list(a_dictionary.values())
+    return list(a_dictionary.keys())[values.index(max)]
