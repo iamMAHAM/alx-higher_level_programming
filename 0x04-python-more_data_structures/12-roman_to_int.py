3e51b0a99b5eac201007fa81d20cc0ed12f0941e
MAHAM <abdulkabore@gmail.com>
2022-06-09 20:13:05 +0200

    remove test

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -26,24 +26,3 @@ def roman_to_int(roman_string):
             behind = number
 
     return output
-
-
-roman_to_int = __import__('12-roman_to_int').roman_to_int
-
-roman_number = "X"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "VII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "IX"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "LXXXVII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "DCCVII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "XXI"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
