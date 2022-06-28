0d769ba954b8cbf29f22898a2a75a62d454f37e3
MAHAM <abdulkabore@gmail.com>
2022-06-28 12:30:53 +0000

    add 0-integer.py

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Add integer module """
+
+
+def add_integer(a, b=98):
+    """ Function that add two numbers """
+
+    if type(a) != int and type(a) != float:
+        raise TypeError("a must be an integer")
+
+    if type(b) != int and type(b) != float:
+        raise TypeError("b must be an integer")
+
+    return a + b
