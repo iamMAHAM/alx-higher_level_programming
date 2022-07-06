141499b813b6aafd851696aaf38c1f5afe5d475d
MAHAM <abdulkabore@gmail.com>
2022-07-06 13:39:06 +0200

    fix error

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -5,6 +5,7 @@
 def add_attribute(obj, name, value):
     """Add or update and atrribute of an object"""
     if not hasattr(obj, "__dict__"):
-        raise TypeError("can\"t add new attribute")
+        raise TypeError("can't add new attribute")
 
     setattr(obj, name, value)
+
