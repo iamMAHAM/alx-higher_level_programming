93c40bd6bf817029cd85f3ea2481bf9c603b8f57
MAHAM <abdulkabore@gmail.com>
2022-07-06 16:58:51 +0200

    fix pycodestyle

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 """Module 7"""
-
-
 import sys
-save_to_json_file = __import__("5-save_to_json_file" ).save_to_json_file
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 try:
     data = load_from_json_file("add_item.json")
-except:
+except Exception:
     data = []
 
 save_to_json_file(data + sys.argv[1:], "add_item.json")
