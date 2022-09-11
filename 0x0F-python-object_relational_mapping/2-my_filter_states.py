da83ffac5449e8412a6969aae0e5a4f74ad16b23
iamMAHAM <abdulkabore@gmail.com>
2022-09-11 18:38:50 +0000

    right sql query

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,9 +11,11 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3], charset="utf8")
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    cursor.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY \
+    id ASC".format(argv[4]))
     states = cursor.fetchall()
     for (i, v) in states:
-        print((i, v)) if v == argv[4] else 0
+        if v == argv[4]:
+            print((i, v))
     cursor.close()
     db.close()
