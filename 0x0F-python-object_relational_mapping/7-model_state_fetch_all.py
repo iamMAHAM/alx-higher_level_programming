9b25f0757cdf8834bdf7006732e0a0637a29f91f
iamMAHAM <abdulkabore@gmail.com>
2022-09-11 20:14:16 +0000

    fix pycodestyle error

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -10,7 +10,8 @@ from sys import argv
 from model_state import Base, State
 
 if __name__ == "__main__":
-    eng = create_engine(f'mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/{argv[3]}')
+    eng = create_engine(f'mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/\
+                        {argv[3]}')
     Base.metadata.create_all(eng)
     Session = sessionmaker(bind=eng)
     session = Session()
