e45737a072351e30139e2631f3543f9cc2c430df
iamMAHAM <abdulkabore@gmail.com>
2022-09-23 17:39:47 +0000

    fix errors

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 """
     Take your Github credentials (username and password) and uses
@@ -12,5 +11,6 @@ from sys import argv
 if __name__ == "__main__":
     user = argv[1]
     passw = argv[2]
-    r = requests.get('https://api.github.com/user', auth=(HTTPBasicAuth(user, passw)))
+    r = requests.get('https://api.github.com/user',
+                     auth=(HTTPBasicAuth(user, passw)))
     print(r.json().get("id"))
