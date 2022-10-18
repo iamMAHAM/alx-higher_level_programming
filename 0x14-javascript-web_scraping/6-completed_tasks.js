8ee9203ad569218e73cc864f6f7c3195ddf9ebb9
iamMAHAM <abdulkabore@gmail.com>
2022-10-18 10:20:14 +0000

    fix all semistandard

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+
+request.get(url, (err, _, body) => {
+  if (err) return;
+  const todos = JSON.parse(body);
+  const userCompletedList = todos.filter(u => u?.completed);
+  const filtered = userCompletedList.map(u => {
+    return u.id;
+  });
+  console.log(Object.assign({}, filtered));
+});
