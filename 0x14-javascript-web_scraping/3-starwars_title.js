d12410eb645a6a8fdfec88cc17ce2257b19600e0
iamMAHAM <abdulkabore@gmail.com>
2022-10-18 09:36:01 +0000

    3-star wars

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const request = require('request');
+const id = process.argv[2];
+request.get(`https://swapi-api.hbtn.io/api/films/${id}`, (err, res, body) => {
+  body = body ? JSON.parse(body) : null;
+  console.log(body?.title || err);
+});
