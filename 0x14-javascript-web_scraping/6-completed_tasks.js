73c76416f51f873b1cad41d62c9596f8f57411be
iamMAHAM <abdulkabore@gmail.com>
2022-10-18 14:48:48 +0000

    remove '?' moderne js operator

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -6,7 +6,7 @@ const url = process.argv[2];
 request.get(url, (err, _, body) => {
   if (err) return;
   const todos = JSON.parse(body);
-  const userCompletedList = todos.filter(u => u?.completed);
+  const userCompletedList = todos.filter(u => u.completed);
   const filtered = userCompletedList.map(u => {
     return u.id;
   });
