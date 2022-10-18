5ca7581e56170050765eee5f8a21b139b63f527e
iamMAHAM <abdulkabore@gmail.com>
2022-10-18 14:53:54 +0000

    move to vanilla js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -4,11 +4,15 @@ const request = require('request');
 const url = process.argv[2];
 
 request.get(url, (err, _, body) => {
+  const obj = {}
   if (err) return;
   const todos = JSON.parse(body);
   const userCompletedList = todos.filter(u => u.completed);
   const filtered = userCompletedList.map(u => {
     return u.id;
   });
-  console.log(Object.assign({}, filtered));
+  for (let i = 0; i < filtered.length; i++){
+    obj[i+1] = filtered[i]
+  }
+  console.log(obj)
 });
