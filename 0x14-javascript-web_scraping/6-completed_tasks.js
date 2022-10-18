f330790c53940ebe34b60dd11c99e0234859fbd4
iamMAHAM <abdulkabore@gmail.com>
2022-10-18 18:54:25 +0000

    6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,18 +1,26 @@
 #!/usr/bin/node
 
+
 const request = require('request');
 const url = process.argv[2];
 
-request.get(url, (err, _, body) => {
-  const obj = {};
-  if (err) return;
-  const todos = JSON.parse(body);
-  const userCompletedList = todos.filter(u => u.completed);
-  const filtered = userCompletedList.map(u => {
-    return u.id;
-  });
-  for (let i = 0; i < filtered.length; i++) {
-    obj[i + 1] = filtered[i];
+const userList = {};
+
+request(url, (err, _, body) => {
+  const list = JSON.parse(body);
+  if (err) {
+    console.log(err);
+  } else {
+    for (let i = 0; i < list.length; i++) {
+      const key = list[i].userId;
+      if (list[i].completed) {
+        if (!userList[key]) {
+          userList[key] = 1;
+        } else {
+          userList[key] += 1;
+        }
+      }
+    }
+    console.log(userList);
   }
-  console.log(obj);
 });
