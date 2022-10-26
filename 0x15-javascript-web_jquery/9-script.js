2e605d27d97b47f3eb29045a377eadb3ef50a0cb
iamMAHAM <abdulkabore@gmail.com>
2022-10-26 10:10:15 +0000

    9 script

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,10 @@
+
+$('document').ready(() => {
+  $.ajax({
+    url: 'https://fourtonfish.com/hellosalut/?lang=fr',
+    dataType: 'json',
+    success: (result) => {
+      $('DIV#hello').text(result.hello);
+    }
+  });
+});
